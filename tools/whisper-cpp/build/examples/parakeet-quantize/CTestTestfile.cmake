748324e5b02549837fbed403347355000f619e7c
Cosm1cAC <cosm1cac@github.com>
2026-07-09 12:33:36 +0800

    chore: stop tracking whisper.cpp artifacts

--- tools/whisper-cpp/build/examples/parakeet-quantize/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/22771/Documents/移动版agent终端/tools/whisper-cpp/source/examples/parakeet-quantize
-# Build directory: C:/Users/22771/Documents/移动版agent终端/tools/whisper-cpp/build/examples/parakeet-quantize
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- tools/whisper-cpp/build/examples/quantize/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/22771/Documents/移动版agent终端/tools/whisper-cpp/source/examples/quantize
-# Build directory: C:/Users/22771/Documents/移动版agent终端/tools/whisper-cpp/build/examples/quantize
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
